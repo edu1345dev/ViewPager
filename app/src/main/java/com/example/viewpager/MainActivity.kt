7d8723642cb8560ec064d2035e2419ace57b0f39
Zé <1e2d3c9a45>
2021-01-30 18:28:33 -0300

    add indicator view

--- app/src/main/java/com/example/viewpager/MainActivity.kt
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -1,33 +1,41 @@
 package com.example.viewpager
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.viewpager.ui.main.PlaceholderFragment
 import com.example.viewpager.ui.main.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.rd.PageIndicatorView
 
 class MainActivity : AppCompatActivity() {
 
     val viewPager by lazy { findViewById<ViewPager2>(R.id.view_pager) }
     val tabs by lazy { findViewById<TabLayout>(R.id.tabs) }
+    val indicator by lazy { findViewById<PageIndicatorView>(R.id.pageIndicator) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val fragments = getFragments()
+
         val sectionsPagerAdapter = SectionsPagerAdapter(fragments, this)
 
         viewPager.adapter = sectionsPagerAdapter
-        viewPager.offscreenPageLimit = 1
+
+        indicator.count = fragments.size
+
+        viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    indicator.setSelected(position)
+                }
+            })
 
         TabLayoutMediator(tabs, viewPager) { tab, position ->
             tab.text = fragments[position].arguments?.getString(
